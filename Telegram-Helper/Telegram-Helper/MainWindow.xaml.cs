e4e889e667bdec31a5091f5d95916786e3aae902
Андрей <zubronov-a@mail.ru>
2021-02-07 16:35:39 +0300

    Переход с net.core обратно на .net framework

--- Telegram-Helper/Telegram-Helper/MainWindow.xaml.cs
+++ Telegram-Helper/Telegram-Helper/MainWindow.xaml.cs
@@ -1,64 +1,28 @@
 ﻿using System;
-using System.IO;
-using System.Net;
-using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Telegram_Helper
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public class SetingTelegram
-        {
-            public string Token { get; set; }
-            public string ChatId { get; set; }
-        }
-
-
         public MainWindow()
         {
             InitializeComponent();
-
         }
-
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-            string token, chatId, text;
-
-            try
-            {
-                using (FileStream fs = new FileStream("setings.json", FileMode.OpenOrCreate))
-                {
-                    SetingTelegram setingTelegram = await JsonSerializer.DeserializeAsync<SetingTelegram>(fs);
-                    token = setingTelegram.Token;
-                    chatId = setingTelegram.ChatId;
-                }
-                text = "https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatId + "&text=" +
-                       "<b>Имя пользователя: </b><i> " + Environment.UserName + "</i>\n" +
-                       "<b>Имя компьютера:</b> <i>" + Environment.MachineName + "</i>>\n" +
-                       "<b>Текст сообжения: </b><i>" + TextBox.Text + "</i>" +
-                       "&parse_mode=html";
-
-                WebRequest request = WebRequest.Create(text);
-                WebResponse response = await request.GetResponseAsync();
-                await using (Stream stream = response.GetResponseStream())
-                {
-                    using StreamReader reader = new StreamReader(stream ?? throw new InvalidOperationException());
-                    reader.ReadToEnd();
-                }
-                response.Close();
-                MessageBox.Show("Сообщение отправлено");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ошибка: " + ex.Message + "\n Свяжитесь с администарторм сетей");
-            }
-
-        }
-
     }
 }
